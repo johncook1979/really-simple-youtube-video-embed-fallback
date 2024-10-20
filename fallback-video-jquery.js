79107ebb4e13b34f1d1d5feb67d88a5027aee521
John Cook <41190295+johncook1979@users.noreply.github.com>
2024-10-21 08:17:25 +1100

    Update fallback-video-jquery.js

--- fallback-video-jquery.js
+++ fallback-video-jquery.js
@@ -1,19 +1,21 @@
+const url = 'https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ&format=json';
+
 $.get(url)
     .done(function(response) {
         console.log('Success:', response);
         
-        // Check if the response contains a valid property (e.g., "html") indicating the video is available
+        // Check if the response contains a valid property indicating the video is available
         if (response && response.html) {
             console.log('Video available');
         } else {
             console.log('Video not available');
-			      $('#fallback-video').show();
-			      $('#main-video').hide();
+		$('#fallback-video').show();
+		$('#main-video').hide();
         }
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
         console.log('Error:', textStatus);
         console.log('Video not available');
-		    $('#fallback-video').show();
-		    $('#main-video').hide();
+	$('#fallback-video').show();
+	$('#main-video').hide();
     });
